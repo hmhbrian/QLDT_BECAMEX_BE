a003929900d54022398238a9b54ae99f7fb57d3c
Huỳnh Hữu Nghĩa <nghiabatuoidev@gmail.com>
2025-06-16 14:33:06 +0700

    update source

--- QLDT_Becamex/Src/Models/Department.cs
+++ QLDT_Becamex/Src/Models/Department.cs
@@ -21,6 +21,6 @@ namespace QLDT_Becamex.Src.Models
 
         public ICollection<ApplicationUser>? Users { get; set; } // Collection của các User thuộc phòng ban này
         public ICollection<Department>? Children { get; set; } // THÊM MỚI: Để quản lý các phòng ban con
-        public ICollection<CourseDepartment> CourseDepartments { get; set; } = new List<CourseDepartment>(); // Danh sách liên kết nhiều-nhiều với Course
+
     }
 }

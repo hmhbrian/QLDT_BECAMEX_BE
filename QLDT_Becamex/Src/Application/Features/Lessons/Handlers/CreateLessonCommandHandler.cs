3620837c998ab2a86fafea0581aef05e57bd3b37
hmhbrian <hoanghm121103@gmail.com>
2025-07-09 15:57:15 +0700

    feat(Lesson): Fix Add&UpdateLesson

--- QLDT_Becamex/Src/Application/Features/Lessons/Handlers/CreateLessonCommandHandler.cs
+++ QLDT_Becamex/Src/Application/Features/Lessons/Handlers/CreateLessonCommandHandler.cs
@@ -1,12 +1,15 @@
 ﻿using MediatR;
 using QLDT_Becamex.Src.Application.Features.Lessons.Commands;
 using QLDT_Becamex.Src.Domain.Entities;
+using PdfSharpCore.Pdf;
 using QLDT_Becamex.Src.Infrastructure.Persistence; // Namespace cho IUnitOfWork
 using QLDT_Becamex.Src.Infrastructure.Services; // Namespace cho CloudinaryService (nếu dùng trực tiếp hoặc service cụ thể)
 using QLDT_Becamex.Src.Domain.Interfaces; // Namespace cho IBaseService và ICloudinaryService
 using QLDT_Becamex.Src.Application.Common.Dtos; // Namespace cho AppException (nếu có)
 using QLDT_Becamex.Src.Application.Features.Lessons.Dtos;
-using QLDT_Becamex.Src.Infrastructure.Services.CloudinaryServices; // Để đảm bảo dùng đúng CreateLessonDto
+using QLDT_Becamex.Src.Infrastructure.Services.CloudinaryServices;
+using PdfSharpCore.Pdf.IO;
+// Để đảm bảo dùng đúng CreateLessonDto
 // using Microsoft.AspNetCore.Http; // Không cần thiết nếu BaseService xử lý HttpContextAccessor
 
 namespace QLDT_Becamex.Src.Application.Features.Lessons.Handlers
@@ -44,33 +47,47 @@ namespace QLDT_Becamex.Src.Application.Features.Lessons.Handlers
             //Lấy position tối đa hiện tại của các bài học trong khóa học
             int maxPosition = await _unitOfWork.LessonRepository.GetMaxPositionAsync(request.CourseId);
 
-            // --- 1. Tải file PDF lên Cloudinary ---
-            string pdfUrl = null!;
+            string Url = null!;
             string filePublicId = null!;
+            int totalDurationSeconds = 0; // biến tổng thời gian video
+            int totalPages = 0; // biến tổng số trang của PDF
 
-            if (request.Request.FilePdf != null && request.Request.FilePdf.Length > 0)
+            if (request.Request.Link != null && request.Request.TotalDurationSeconds > 0 && request.Request.FilePdf == null) // Kiểm tra nếu là link
             {
-
-                var uploadResult = await _cloudinaryService.UploadPdfAsync(request.Request.FilePdf, "Lesson_pdfs");
-                pdfUrl = uploadResult?.url!; // Lấy URL từ kết quả upload
-                filePublicId = uploadResult?.publicId!; // Lấy PublicId từ kết quả upload
-
-                if (string.IsNullOrEmpty(pdfUrl))
+                
+                Url = request.Request.Link!; // Lấy URL từ request
+                totalDurationSeconds = request.Request.TotalDurationSeconds; // Lấy tổng thời gian của video
+            }
+            else if (request.Request.FilePdf != null && request.Request.FilePdf.Length > 0)
+            {
+                using (var stream = request.Request.FilePdf.OpenReadStream())
                 {
-                    throw new AppException("Failed to upload PDF file to Cloudinary.", 500);
-                }
+                    using (var pdfDocument = PdfReader.Open(stream, PdfDocumentOpenMode.ReadOnly))
+                    {
+                        totalPages = pdfDocument.PageCount; // Lấy tổng số trang của PDF
+
+                        // Upload lên Cloudinary
+                        var uploadResult = await _cloudinaryService.UploadPdfAsync(request.Request.FilePdf, "Lesson_pdfs");
+                        Url = uploadResult?.url!; // Lấy URL từ kết quả upload
+                        filePublicId = uploadResult?.publicId!; // Lấy PublicId từ kết quả upload
+
+                        if (string.IsNullOrEmpty(Url))
+                        {
+                            throw new AppException("Failed to upload PDF file to Cloudinary.", 500);
+                        }
+                    }
+                }    
             }
             else
             {
                 // Nếu file PDF là bắt buộc, hãy ném ngoại lệ
-                throw new AppException("PDF file is required for creating a lesson.", 400); // Mã 400 cho Bad Request
+                throw new AppException("Creating a lesson must have Pdf file or link video.", 400); // Mã 400 cho Bad Request
             }
-
 
             // Nếu bạn vẫn đang sử dụng phương thức `Create` trên instance `Lesson`:
             var lesson = new Lesson();
 
-            lesson.Create(request.CourseId, userId, request.Request, pdfUrl, filePublicId, maxPosition+1);
+            lesson.Create(request.CourseId, userId, request.Request, Url, filePublicId, maxPosition+1, totalDurationSeconds, totalPages);
 
             // --- 3. Thêm entity vào Repository và lưu vào DB ---
             await _unitOfWork.LessonRepository.AddAsync(lesson);
--- QLDT_Becamex/Src/Domain/Entities/Lesson.cs
+++ QLDT_Becamex/Src/Domain/Entities/Lesson.cs
@@ -1,4 +1,5 @@
 ﻿
+using Castle.Components.DictionaryAdapter.Xml;
 using QLDT_Becamex.Src.Application.Features.Lessons.Dtos;
 using QLDT_Becamex.Src.Shared.Helpers;
 namespace QLDT_Becamex.Src.Domain.Entities
@@ -24,7 +25,7 @@ namespace QLDT_Becamex.Src.Domain.Entities
         public DateTime? UpdatedAt { get; set; }
         public ICollection<LessonProgress>? LessonProgress { get; set; } = new List<LessonProgress>();
 
-        public void Create(string courseId, string userIdCreated, CreateLessonDto request, string urlPdf, string filePublicId, int position)
+        public void Create(string courseId, string userIdCreated, CreateLessonDto request, string urlPdf, string filePublicId, int position, int totalDurations, int totalPages)
         {
             Title = request.Title.ToLower().Trim();
             FileUrl = urlPdf;
@@ -32,21 +33,48 @@ namespace QLDT_Becamex.Src.Domain.Entities
             CourseId = courseId;
             Position = position;
             UserIdCreated = userIdCreated;
+
+            if(totalDurations > 0)   
+                TotalDurationSeconds = totalDurations; // Lưu tổng thời gian của bài học
+            else
+                TotalPages = totalPages;//lưu tổng số trang của PDF
+
+            TypeDocId = request.TypeDocId; 
             CreatedAt = DateTime.Now;
             UpdatedAt = DateTime.Now;
         }
 
-        public void Update(string courseId, string userIdEdited, UpdateLessonDto request, string fileUrl, string newFilePublicId)
+        public void Update(string courseId, string userIdEdited, UpdateLessonDto request, string urlPdf, string newFilePublicId, int totalDurations, int totalPages)
         {
             if (!string.IsNullOrWhiteSpace(request.Title) && request.Title != Title)
                 Title = request.Title;
 
-            if (!string.IsNullOrWhiteSpace(fileUrl) && fileUrl != FileUrl)
-                FileUrl = fileUrl;
+            if (!string.IsNullOrWhiteSpace(urlPdf) && urlPdf != FileUrl)
+                FileUrl = urlPdf;
 
-            if (!string.IsNullOrWhiteSpace(newFilePublicId) && PublicIdUrlPdf != newFilePublicId)
-                PublicIdUrlPdf = newFilePublicId;
+            // Kiểm tra và xóa PublicIdUrlPdf nếu newFilePublicId là null hoặc TypeDocId là 2
+            if (newFilePublicId == null || request.TypeDocId == 2)
+            {
+                PublicIdUrlPdf = null; // Xóa dữ liệu cũ của PublicIdUrlPdf
+            }
+            else if (!string.IsNullOrWhiteSpace(newFilePublicId) && PublicIdUrlPdf != newFilePublicId)
+            {
+                PublicIdUrlPdf = newFilePublicId; // Cập nhật nếu khác và không null
+            }
 
+
+            if (totalDurations > 0)
+            {
+                TotalDurationSeconds = totalDurations; // Lưu tổng thời gian của bài học
+                TotalPages = null; // Đặt TotalPages là null nếu có tổng thời gian
+            }
+            else
+            {
+                TotalPages = totalPages;//lưu tổng số trang của PDF
+                TotalDurationSeconds = null; // Đặt TotalDurationSeconds là null nếu có tổng số trang
+            }
+             
+            TypeDocId = request.TypeDocId;
 
             if (courseId != CourseId)
                 CourseId = courseId;

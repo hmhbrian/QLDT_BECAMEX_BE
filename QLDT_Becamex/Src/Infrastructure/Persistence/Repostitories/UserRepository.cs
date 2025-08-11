bc15f0007247e49ebb4dbdfcedb74ab9389e4d6e
hmhbrian <hoanghm121103@gmail.com>
2025-08-11 12:19:35 +0700

    feat(User): search user

--- QLDT_Becamex/Src/Infrastructure/Persistence/Repostitories/UserRepository.cs
+++ QLDT_Becamex/Src/Infrastructure/Persistence/Repostitories/UserRepository.cs
@@ -1,13 +1,50 @@
-﻿using QLDT_Becamex.Src.Domain.Interfaces;
-using QLDT_Becamex.Src.Domain.Entities;
+﻿using QLDT_Becamex.Src.Domain.Entities;
+using QLDT_Becamex.Src.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 
 namespace QLDT_Becamex.Src.Infrastructure.Persistence.Repostitories
 {
     public class UserRepository : GenericRepository<ApplicationUser>, IUserRepository
     {
+        protected readonly ApplicationDbContext _dbContext;
         public UserRepository(ApplicationDbContext dbContext) : base(dbContext)
         {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<ApplicationUser> GetFlexible(
+            Expression<Func<ApplicationUser, bool>> predicate,
+            Func<IQueryable<ApplicationUser>, IOrderedQueryable<ApplicationUser>>? orderBy,
+            int page,
+            bool asNoTracking,
+            Expression<Func<ApplicationUser, object>>[]? includes)
+        {
+            var query = _dbContext.Users.AsQueryable();
+
+            if (asNoTracking)
+                query = query.AsNoTracking();
+
+            if (predicate != null)
+                query = query.Where(predicate);
+
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            if (orderBy != null)
+                query = orderBy(query);
+
+            // Phân trang
+            int limit = 10; // Giả sử limit mặc định
+            query = query.Skip((page - 1) * limit).Take(limit);
+
+            return query;
         }
     }
 }
